4d5841ed506b33748ec33b93643c9c6d7de3e8f5
Amita Narayanan Kutty <116826376+Amita-NK@users.noreply.github.com>
2023-12-16 00:00:58 +0530

    UID Project Uploaded.
    Team Members - 42 & 67

---
+++ Assignments/CB.EN.U4CYS22067/ui/js/script.js
@@ -0,0 +1,26 @@
+function validateForm() {
+    var fullName = document.getElementById("fullName").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var email = document.getElementById("email").value;
+  
+    if (!/^\d{10}$/.test(phoneNumber)) {
+      alert("Invalid phone number. Please enter a 10-digit number.");
+      return false;
+    }
+  
+    if (!email.includes("@")) {
+      alert("Invalid email address. Please enter a valid email.");
+      return false;
+    }
+  
+    return true;
+  }
+function redirectToThanks() {
+    window.location.href = "../html/thanks.html";
+}
+
+function submitForm() {
+if (validateForm()) {
+  redirectToThanks();
+}
+}  
