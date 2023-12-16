e26a7ce298d8284639b953928393cd5588026f2b
Jose Rohit M <116860412+joserohit264@users.noreply.github.com>
2023-12-16 13:26:33 +0530

    28 and 30

---
+++ Assignments/CB.EN.U4CYS22030/ui/script/signinpage.js
@@ -0,0 +1,27 @@
+        document.getElementById('signinForm').addEventListener('submit', function (event) {
+            event.preventDefault();
+            if (validateSignIn()) {
+                alert('Login successful!');
+                
+                window.location.href = 'main.html';
+            } else {
+                alert('Login failed. Please check your username and password.');
+            }
+        });
+
+        function validateSignIn() {
+            
+            var username = document.getElementById('signinUsername').value;
+            var password = document.getElementById('signinPassword').value;
+
+            
+            var validUsername = 'hemanth_2503';
+            var validPassword = 'abc@123';
+
+            
+            if (username === validUsername && password === validPassword) {
+                return true;
+            } else {
+                return true;
+            }
+        }
---
+++ Assignments/CB.EN.U4CYS22030/ui/script/signuppage.js
@@ -0,0 +1,30 @@
+        document.getElementById('signupForm').addEventListener('submit', function (event) {
+            event.preventDefault();
+            if (validateSignUp()) {
+                
+                window.location.href = 'SignInPage.html';
+            }
+        });
+
+        function validateSignUp() {
+
+            var username = document.getElementById('username').value;
+            
+            var age = document.getElementById('age').value;
+            var password = document.getElementById('password').value;
+            var confirmPassword = document.getElementById('confirmPassword').value;
+
+            if (!username  || !age || !password || !confirmPassword) {
+                alert('Please fill out all fields.');
+                return false;
+            }
+
+            if (password !== confirmPassword) {
+                alert('Passwords do not match.');
+                return false;
+            }
+
+        
+
+            return true;
+        }
