a077cadbda518aa657da8dbda410ce39578392cb
Asrita <116860068+luckyasrita-16@users.noreply.github.com>
2023-12-17 16:29:27 +0530

    Update home.js

--- Assignments/CB.EN.U4CYS22009/ui/js/home.js
+++ Assignments/CB.EN.U4CYS22009/ui/js/home.js
@@ -1,70 +1,70 @@
 function goToAnotherPage() {
     // Redirect to the desired page
-    window.location.href = "../../html/learnhtml.html";
+    window.location.href = "../html/learnhtml.html";
   }
   function goToAnotherPage1()
 {
-    window.location.href="../../html/referencehtml.html";
+    window.location.href="../html/referencehtml.html";
 }
 function goToAnotherPage2()
 {
-    window.location.href="../../html/htmlcertificate.html";
+    window.location.href="../html/htmlcertificate.html";
 }
 function cssPage()
 {
-    window.location.href="../../html/csslearn.html";
+    window.location.href="../html/csslearn.html";
 }
 function cssPage1()
 {
-    window.location.href="../../html/referencecss.html";
+    window.location.href="../html/referencecss.html";
 }
 function cssPage2()
 {
-    window.location.href="../../html/csscertificate.html";
+    window.location.href="../html/csscertificate.html";
 }
 function javaPage()
 {
-    window.location.href="../../html/javascriptlearn.html"
+    window.location.href="../html/javascriptlearn.html"
 }
 function javaPage1()
 {
-    window.location.href="../../html/javascriptreference.html";
+    window.location.href="../html/javascriptreference.html";
 }
 function javaPage2()
 {
-    window.location.href="../../html/javascriptcertificate.html";
+    window.location.href="../html/javascriptcertificate.html";
 }
 function PythonPage()
 {
-    window.location.href="../../html/phytonlearn.html"
+    window.location.href="../html/phytonlearn.html"
 }
 function PythonPage1()
 {
-    window.location.href="../../html/phytonreference.html";
+    window.location.href="../html/phytonreference.html";
 }
 function PythonPage2()
 {
-    window.location.href="../../html/phytoncertificate.html";
+    window.location.href="../html/phytoncertificate.html";
 }
 function SQLPage()
 {
-    window.location.href="../../html/aqllearn.html";
+    window.location.href="../html/aqllearn.html";
 }
 function SQLPage1()
 {
-    window.location.href="../../html/sqlreference.html";
+    window.location.href="../html/sqlreference.html";
 }
 function SQLPage2()
 {
-    window.location.href="../../html/sqlcertificate.html";
+    window.location.href="../html/sqlcertificate.html";
 }
 function SignUPPage()
 {
-  window.location.href="../../html/signup1.html";
+  window.location.href="../html/signup1.html";
 }
 function LoginPage()
 {
-    window.location.href="../../html/login.html";
+    window.location.href="../html/login.html";
 }
 function htmllink() {
   document.getElementById("HTMLlinking").scrollIntoView();
