017f9357b957b254aab9ccfa5d17645abd4dd053
LOGESH R <116827815+Logesh0107@users.noreply.github.com>
2023-12-29 13:29:51 +0530

    Add files via upload

---
+++ Assignments/CB.EN.U4CYS22036/ui/JAVASCRIPT/signupscript.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+  // Prevent the form from submitting (for demonstration)
+  event.preventDefault();
+
+  // You can add JavaScript functionality here
+  // For example, you can perform form validation or AJAX request
+  alert('Sign-up form submitted!'); // This is just an example alert
+});
