149119ea11698e88abe2b7665110cc3e780d4a8a
Jose Rohit M <116860412+joserohit264@users.noreply.github.com>
2024-01-02 20:51:44 +0530

    Update signuppage.js

--- Assignments/CB.EN.U4CYS22030/ui/script/signuppage.js
+++ Assignments/CB.EN.U4CYS22030/ui/script/signuppage.js
@@ -2,7 +2,7 @@
             event.preventDefault();
             if (validateSignUp()) {
                 
-                window.location.href = 'SignInPage.html';
+                window.location.href = '../html/signInPage.html';
             }
         });
 
